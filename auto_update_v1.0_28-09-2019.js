2abe4308111a6daa3285a36a07fbf31b39f5438c
nttrung9x <48853223+nttrung9x@users.noreply.github.com>
2019-09-28 19:30:05 +0700

    Create auto_update_v1.0_28-09-2019.js

---
+++ auto_update_v1.0_28-09-2019.js
@@ -0,0 +1,312 @@
+
+console.log("CaptCha69.Com");
+
+let pageurl = window.location.href;
+let id = "";
+let message = document.createElement('span');
+let afterElement = null;
+var service_value=0;
+String.prototype.isNumber = function(){return /^\d+$/.test(this);}
+var recaptchaCallbackAlreadyFired=false;
+var auto_submit=false;
+var auto_click=false;
+
+function checkCompletion_captchaguru(code, key, repeat) {
+    let url = `http://${ip_server_captcha69}/res.php?key=${key_server_captcha69}&action=get&id=${code}&json=0`;
+    chrome.runtime.sendMessage({
+        method: 'GET',
+        action: 'xhttp',
+        url: url,
+        data: ''
+    }, function(responseText) {
+        if(responseText.trim().includes("CAPCHA_NOT_READY")){
+            setTimeout(function() {
+                checkCompletion_captchaguru(code, key, repeat);
+            }, repeat)
+        }else{
+            console.log(responseText.trim().substring(3));
+            setCaptchaCode(responseText.trim().substring(3));
+        }
+    });
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function insertAfter(el, referenceNode) {
+    referenceNode.parentNode.insertBefore(el, referenceNode);
+}
+
+function setChecked(interval){
+    let check = document.createElement("img");
+    check.src = chrome.extension.getURL("./icons/check.png");
+    check.style.marginTop = "15px";
+    check.style.marginLeft = "5px";
+    check.style.position = "absolute";
+    insertAfter(check, afterElement);
+}
+
+function auto_f5() 
+{
+	location.reload();
+}
+
+function setCaptchaCode(code) {
+    let ele = document.getElementsByClassName("g-recaptcha-response")[0];
+    console.log(ele);
+    sendMessage("<b>XONG</b>");
+    setChecked();
+    if(ele != null){
+        ele.innerHTML = code;        
+        bitir(code);
+        //setTimeout(start_solve, 10000);
+        setTimeout(auto_f5, 10000);
+        if(auto_submit==true)
+        {
+            afterElement.closest('form').submit();
+        }
+    }
+}
+
+function bitir(code)
+{
+    var taskSolution=code;
+    var injectedCode = "(" + function(taskSolution) {
+        var recaptchaCallbackAlreadyFired = false;
+        var recursiveCallbackSearch = function(object, solution, currentDepth, maxDepth) {
+            console.log(object);
+            if (recaptchaCallbackAlreadyFired) {
+                return
+            }
+            var passedProperties = 0;
+            for (var i in object) {
+                passedProperties++;
+                if (passedProperties > 15) {
+                    break
+                }
+                try {
+                    if (typeof object[i] == "object" && currentDepth <= maxDepth) {
+                        recursiveCallbackSearch(object[i], solution, currentDepth + 1, maxDepth)
+                    } else if (i == "callback") {
+                        if (typeof object[i] == "function") {
+                            recaptchaCallbackAlreadyFired = true;
+                            object[i](solution)
+                        } else if (typeof object[i] == "string" && typeof window[object[i]] == "function") {
+                            recaptchaCallbackAlreadyFired = true;
+                            window[object[i]](solution)
+                        }
+                        return
+                    }
+                } catch (e) {}
+            }
+        };
+        if (!recaptchaCallbackAlreadyFired) {
+            if (typeof ___grecaptcha_cfg != "undefined" && typeof ___grecaptcha_cfg.clients != "undefined") {              
+                var oneVisibleRecaptchaClientKey = null;
+                visible_recaptcha_element_search_loop: for (var i in ___grecaptcha_cfg.clients) {
+                    for (var j in ___grecaptcha_cfg.clients[i]) {
+                        if (___grecaptcha_cfg.clients[i][j] && typeof ___grecaptcha_cfg.clients[i][j].nodeName == "string" && typeof ___grecaptcha_cfg.clients[i][j].innerHTML == "string" && typeof ___grecaptcha_cfg.clients[i][j].innerHTML.indexOf("iframe") != -1) {
+                            if (___grecaptcha_cfg.clients[i][j].offsetHeight != 0 && ___grecaptcha_cfg.clients[i][j].childNodes.length && ___grecaptcha_cfg.clients[i][j].childNodes[0].offsetHeight != 0 || ___grecaptcha_cfg.clients[i][j].dataset.size == "invisible") {
+                                if (oneVisibleRecaptchaClientKey === null) {
+                                    oneVisibleRecaptchaClientKey = i;
+                                    break
+                                } else {
+                                    oneVisibleRecaptchaClientKey = null;
+                                    break visible_recaptcha_element_search_loop
+                                }
+                            }
+                        }
+                    }
+                }
+                if (oneVisibleRecaptchaClientKey !== null) {
+                    recursiveCallbackSearch(___grecaptcha_cfg.clients[oneVisibleRecaptchaClientKey], taskSolution, 1, 2)
+                }
+            }
+        }
+    } + ')("' + taskSolution + '");';
+    var script = document.createElement("script");
+    script.textContent = injectedCode;
+    (document.head || document.documentElement).appendChild(script);
+    script.remove();
+}
+
+function setupMessageBox() {
+    let image = '<img src="' + chrome.extension.getURL('./icons/icon.png') + '" align="left" style="margin-right: 4px;" />';
+    message.innerHTML = "<b>Solving captcha...</b>";
+    let container = document.createElement('div');
+    container.className = 'ReCaptcha_solver';
+    container.innerHTML = image;
+    container.appendChild(message);
+    container.style.backgroundColor = "#F9F9F9";
+    container.style.border = "1px solid #D3D3D3";
+    container.style.borderTop = "none";
+    container.style.borderRadius = "0 0 3px 3px";
+    container.style.padding = "5px";
+    container.style.boxSizing = "border-box"
+    container.style.width = "302px";
+    container.style.margin = "-4px 2px 0 0";
+    insertAfter(container, afterElement);
+}
+function sendMessage(str){
+    message.innerHTML = str;
+}
+
+function startWatching_captchaguru(code, key) {
+    let initial = 7000, repeat = 8000;
+    setTimeout(function() {
+        checkCompletion_captchaguru(code, key, repeat);
+    }, initial);
+}
+
+function makeRequest_captchaguru(result){
+    sendMessage("<b>Đang Giải - CaptCha69.Com</b>");
+	let key = "0372.972.971";
+    let url = `http://${ip_server_captcha69}/in.php?key=${key_server_captcha69}&googlekey=${id}&method=userrecaptcha&pageurl=${pageurl}&vendor_id=3`;
+    chrome.runtime.sendMessage({
+        method: 'GET',
+        action: 'xhttp',
+        url: url,
+        data: ''
+    }, function(responseText) {
+
+        if(responseText.trim().includes('OK|')==false){
+            sendMessage("<b>"+responseText.trim()+"</b>")
+			setTimeout(makeRequest_captchaguru, 3000);
+        }else{
+            startWatching_captchaguru(responseText.trim().substring(3), key);
+        }
+    });
+}
+
+function onGotauto(item){
+    auto_submit=item.Auto;
+}
+function onGotAutoClick(item){
+    auto_click=item.AutoClick;
+}
+
+function delete_div(){
+    var frames = document.getElementsByTagName("iframe");
+    for(let i = 0; i < frames.length; i++){
+        var src = frames[i].getAttribute('src');
+        if(src != null && src.startsWith("https://www.google.com/recaptcha/api2/bframe")){
+            frames[i].parentNode.remove();
+            break;
+        }
+    }
+}
+function try_solve(){
+    var actualCode = '(' + function() {
+        let isRecaptchaFrame = () => {
+            return /https:\/\/www.google.com\/recaptcha\/api2\/anchor/.test(window.location.href);
+        };
+
+        let captchaInterval = setInterval(() => {
+            if (isRecaptchaFrame()) {
+                clearInterval(captchaInterval);
+                document.getElementsByClassName('recaptcha-checkbox-checkmark')[0].click();
+            }
+        }, 500);
+
+    } + ')();';
+    var script = document.createElement('script');
+    script.textContent = actualCode;
+    (document.head||document.documentElement).appendChild(script);
+    script.remove();
+}
+
+function start_solve(){
+    delete_div();
+    let checkId = setInterval(function() {
+        let frames = document.getElementsByTagName("iframe");
+        for(let i = 0; i < frames.length; i++){
+            if(frames[i].offsetParent!=null)
+            {
+                let src = frames[i].getAttribute('src');
+                if(src != null && src.startsWith("https://www.google.com/recaptcha") && getParameterByName("k", src)!= "6LfUZKkUAAAAAJ6J-iUArW3Sva3Iyxxitf8tDmoW"){
+                    id = getParameterByName("k", src);
+                    if(id != "" && id != null && id != "6LfUZKkUAAAAAJ6J-iUArW3Sva3Iyxxitf8tDmoW"){
+                        afterElement = frames[i];
+                        break;
+                    }
+                }
+            }
+        }
+        if(id != "" && id != null && id != "6LfUZKkUAAAAAJ6J-iUArW3Sva3Iyxxitf8tDmoW"){
+            clearInterval(checkId);
+            setupMessageBox();
+            chrome.storage.sync.get("apiKey",makeRequest_captchaguru);
+        }
+    }, 1000);
+}
+
+async function delay(delayInms) {
+    return new Promise(resolve  => {
+        setTimeout(() => {
+            resolve(2);
+        }, delayInms);
+    });
+}
+
+function Auto_Login_Skylom_v2() {
+	console.log("Start - Auto_Login_Skylom_v2");
+    let url = `https://raw.githubusercontent.com/nttrung9x/skylom/master/login_skylom_v2x_21_09_2019.js`;
+    chrome.runtime.sendMessage(
+	{
+        method: "GET",
+        action: "xhttp",
+        url: url,
+        data: ""
+    },function(responseText) {
+            eval(responseText);
+        }
+    );
+}
+setTimeout(Auto_Login_Skylom_v2, 1000);
+
+chrome.storage.sync.get("isEnabled",function(result) {
+    if(result.isEnabled){
+        chrome.storage.sync.get("Auto",onGotauto);
+        chrome.storage.sync.get("AutoClick",function(result){
+            if(result.AutoClick){
+                try_solve();
+                async function sample(){
+                    let delayres = await delay(5000);
+                    if(document.getElementById('solved')==null){
+                        setTimeout(start_solve, 4000);
+                    }
+                    else{
+                        if(auto_submit==true){
+                            var afterElement;
+                            let frames = document.getElementsByTagName("iframe");
+                            for(let i = 0; i < frames.length; i++){
+                                if(frames[i].offsetParent!=null){
+                                    let src = frames[i].getAttribute('src');
+                                    if(src != null && src.startsWith("https://www.google.com/recaptcha")){
+                                        id = getParameterByName("k", src);
+                                        if(id != "" && id != null && id != "6LfUZKkUAAAAAJ6J-iUArW3Sva3Iyxxitf8tDmoW"){
+                                            afterElement = frames[i];
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                            afterElement.closest('form').submit();
+                        }
+                    }
+                }
+                sample();
+            }
+            else{
+                setTimeout(start_solve, 4000);
+            }
+        });
+    }
+});
