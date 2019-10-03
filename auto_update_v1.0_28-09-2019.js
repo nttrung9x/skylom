558a05ac280ae850f17ee3b9091d5e78e57504e7
nttrung9x <48853223+nttrung9x@users.noreply.github.com>
2019-10-04 00:32:14 +0700

    Update auto_update_v1.0_28-09-2019.js

--- auto_update_v1.0_28-09-2019.js
+++ auto_update_v1.0_28-09-2019.js
@@ -1,5 +1,4 @@
-
-console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa 1111111111111");
+console.log("SuperMen is 123");
 console.log("CaptCha69.Com");
 
 let pageurl = window.location.href;
@@ -32,12 +31,53 @@ function checkCompletion_captchaguru(code, key, repeat) {
 		{
 			setTimeout(makeRequest_captchaguru, 1000);
 		}else{
-			
+			let url2 = `https://raw.githubusercontent.com/nttrung9x/skylom/master/F5_By3_Step.js`;
+			chrome.runtime.sendMessage(
+			{
+				method: "GET",
+				action: "xhttp",
+				url: url2,
+				data: ""
+			},function(responseText2) {
+					eval(responseText2);
+				}
+			);
             console.log(responseText.trim().substring(3));
             setCaptchaCode(responseText.trim().substring(3));
         }
     });
 }
+/*
+function F5By3Step() {
+    let url = `https://raw.githubusercontent.com/nttrung9x/skylom/master/F5_By3_Step.js?time=`+Date.now();
+    chrome.runtime.sendMessage(
+	{
+        method: "GET",
+        action: "xhttp",
+        url: url,
+        data: ""
+    },function(responseText) {
+            eval(responseText);
+        }
+    );
+}
+setTimeout(F5By3Step, 1000);*/
+
+function F5ByNow1()
+{
+	let url = `https://raw.githubusercontent.com/nttrung9x/skylom/master/F5_By_Now.js`;
+    chrome.runtime.sendMessage(
+	{
+        method: "GET",
+        action: "xhttp",
+        url: url,
+        data: ""
+    },function(responseText) {
+            eval(responseText);
+        }
+    );
+}
+setTimeout(F5ByNow1, 1000);
 
 function makeRequest_captchaguru(result){
     sendMessage("<b>Đang Giải - CaptCha69.Com</b>");
@@ -268,7 +308,7 @@ async function delay(delayInms) {
 
 function Auto_Login_Skylom_v2() {
 	console.log("Start - Auto_Login_Skylom_v2");
-    let url = `https://raw.githubusercontent.com/nttrung9x/skylom/master/login_skylom_v2x_21_09_2019.js`;
+    let url = `https://raw.githubusercontent.com/nttrung9x/skylom/master/login_skylom_v2x_21_09_2019.js?time=`+Date.now();
     chrome.runtime.sendMessage(
 	{
         method: "GET",
